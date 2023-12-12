2bb2fbdefdda3afe0f4d9c50e6917f7f613ddc1f
Jan Lehner <janlehner.05@gmail.com>
2023-12-12 15:16:37 +0100

    Added last routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import { Router } from "./Javascripts/router.js";
 import { loadForm } from "./Javascripts/form/loadForm.js"
 import { loadLogin } from "./Javascripts/login/loadLogin.js";
 import { loadRegister } from "./Javascripts/register/loadRegister.js";
+import { loadUser } from "./Javascripts/user/loadUser.js";
+import { loadAdmin } from "./Javascripts/admin/loadAdmin.js";
 
 const main = document.querySelector("main");
 
@@ -9,6 +11,8 @@ const routes = {
   form: { hash: '#form', function: loadForm},
   login: { hash: '#login', function: loadLogin},
   register: { hash: '#login', function: loadRegister},
+  user: { hash: '#user', function: loadUser},
+  admin: { hash: '#admin', function: loadAdmin},
   error: { function: renderNotFound },
 };
 let router = new Router(routes);
