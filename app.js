f3b6bcce6064b1efe668c48185020b3e712f6875
Jan Lehner <janlehner.05@gmail.com>
2023-12-12 14:56:32 +0100

    Added additional routes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import { Router } from "./Javascripts/router.js";
 import { loadForm } from "./Javascripts/form/loadForm.js"
+import { loadLogin } from "./Javascripts/login/loadLogin.js";
+import { loadRegister } from "./Javascripts/register/loadRegister.js";
 
 const main = document.querySelector("main");
 
 const routes = {
   form: { hash: '#form', function: loadForm},
+  login: { hash: '#login', function: loadLogin},
+  register: { hash: '#login', function: loadRegister},
   error: { function: renderNotFound },
 };
 let router = new Router(routes);
