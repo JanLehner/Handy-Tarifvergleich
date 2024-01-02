3cbb9f0623590ff28d547dfec6904cbf9f850564
Jan Lehner <janlehner.05@gmail.com>
2024-01-02 21:00:12 +0100

    Design für die Login-Seite erstellt

--- Javascripts/login/loadLogin.js
+++ Javascripts/login/loadLogin.js
@@ -1,1 +1,17 @@
-export function loadLogin() {}
+export function loadLogin() {
+    styleHolder.innerHTML = `<link rel="stylesheet" href="./Stylesheets/loginStyle.css"></link>`;
+    main.innerHTML = login;
+};
+
+const styleHolder = document.getElementById("styleHolder");
+const main = document.querySelector('main');
+const login = `<div class="flexbox mainHeader">
+<a id="switchToRegister" href="#register">Neu hier? Registrieren Sie sich.</a>
+</div>
+<p class="siteInfo">Login</p>
+<div class="flexbox inputDivLogin">
+<input id="inputUsername" class="inputLogin" type="text" placeholder="Benuztername">
+<input id="inputPassword" class="inputLogin" type="password" placeholder="Passwort">
+<p class="errorMessage">Benutzername oder Passwort falsch.</p>
+<a class="flexbox btn" id="btnLogin">Login</a>
+</div>`;
