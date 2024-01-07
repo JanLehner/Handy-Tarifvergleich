298063d8d875e1f3a8186eb7ddb6be4f752d2317
Ellis Arn <ellisarn@gmail.com>
2024-01-07 16:13:10 +0100

    edit offer implementiert

--- Javascripts/detailView/loadDetailView.js
+++ Javascripts/detailView/loadDetailView.js
@@ -28,75 +28,79 @@ async function getOfferData(offerId) {
       <div class="flexbox infoDiv">
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Aboname:</p>
-          <input class="flexbox infoInput" type="text" value="${data.name}" placeholder="string">
+          <input class="flexbox infoInput" id="name" type="text" value="${data.name}" placeholder="string">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Provider:</p>
-          <input class="flexbox infoInput" type="text" value="${data.provider}" placeholder="string">
+          <input class="flexbox infoInput" id="provider" type="text" value="${data.provider}" placeholder="string">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Angebots-URL:</p>
-          <input class="flexbox infoInput" type="url" value="${data.offerUrl}" placeholder="string">
+          <input class="flexbox infoInput" id="offerUrl" type="url" value="${data.offerUrl}" placeholder="string">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Grundpreis:</p>
-          <input class="flexbox infoInput" type="number" value="${data.basePrice}" placeholder="decimal">
+          <input class="flexbox infoInput" id="basePrice" type="number" value="${data.basePrice}" placeholder="decimal">
+      </div>
+      <div class="flexbox infoCard">
+          <p class="flexbox infoTitle">Welt:</p>
+          <input class="flexbox infoInput" id="worldOffer" type="checkbox" value="${data.worldOffer}">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Telefonkosten CH (min):</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.callPerCallminuteCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="callPerCallminuteCH" type="number" value="${data.cost.callPerCallminuteCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Gigabytekosten CH:</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.internetPerGBCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="internetPerGBCH" type="number" value="${data.cost.internetPerGBCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">SMS-Kosten CH:</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.smsPerCountCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="smsPerCountCH" type="number" value="${data.cost.smsPerCountCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Telefonkosten EU (min):</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.callPerCallminuteEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="callPerCallminuteEurope" type="number" value="${data.cost.callPerCallminuteEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Gigabytekosten EU:</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.internetPerGBEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="internetPerGBEurope" type="number" value="${data.cost.internetPerGBEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">SMS-Kosten EU:</p>
-          <input class="flexbox infoInput" type="number" value="${data.cost.smsPerCountEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="smsPerCountEurope" type="number" value="${data.cost.smsPerCountEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Telefonguthaben CH (min):</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeGBInternetCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeGBInternetCH" type="number" value="${data.deductions.freeGBInternetCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Gigabyteguthaben CH:</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeCallminutesCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeCallminutesCH" type="number" value="${data.deductions.freeCallminutesCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">SMS-Guthaben CH:</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeSMSCH}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeSMSCH" type="number" value="${data.deductions.freeSMSCH}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Telefonguthaben EU (min):</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeGBInternetEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeGBInternetEurope" type="number" value="${data.deductions.freeGBInternetEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Gigabyteguthaben EU:</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeCallminutesEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeCallminutesEurope" type="number" value="${data.deductions.freeCallminutesEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">SMS-Guthaben EU:</p>
-          <input class="flexbox infoInput" type="number" value="${data.deductions.freeSMSEurope}" placeholder="decimal">
+          <input class="flexbox infoInput" id="freeSMSEurope" type="number" value="${data.deductions.freeSMSEurope}" placeholder="decimal">
       </div>
       <div class="flexbox infoCard">
           <p class="flexbox infoTitle">Aktivierungsgebühr:</p>
-          <input class="flexbox infoInput" type="number" value="${data.activationFee}" placeholder="decimal">
+          <input class="flexbox infoInput" id="activationFee" type="number" value="${data.activationFee}" placeholder="decimal">
       </div>
       </div>
       <div class="flexbox saveBtnDiv">
-      <p class="errorMessage">Füllen Sie alle Felder aus.</p>
+      <p class="errorMessage" hidden>Füllen Sie alle Felder aus.</p>
       </div>
       <div class="flexbox saveBtnDiv">
       <a class="flexbox btn saveBtn">Speichern</a>
@@ -105,5 +109,91 @@ async function getOfferData(offerId) {
         main.innerHTML = ''
         window.location.hash = '#offer'
       })
+      document.querySelector('.saveBtn').addEventListener('click', () => {
+        saveOfferData(offerId)
+      })
+      document.querySelector('#worldOffer').checked = data.worldOffer
     })
+}
+
+async function saveOfferData(offerId) {
+  const main = document.querySelector('main')
+  const name = document.getElementById('name').value
+  const provider = document.getElementById('provider').value
+  const offerUrl = document.getElementById('offerUrl').value
+  const basePrice = document.getElementById('basePrice').value
+  const worldOffer = document.getElementById('worldOffer').value
+  const callPerCallminuteCH = document.getElementById(
+    'callPerCallminuteCH'
+  ).value
+  const internetPerGBCH = document.getElementById('internetPerGBCH').value
+  const smsPerCountCH = document.getElementById('smsPerCountCH').value
+  const callPerCallminuteEurope = document.getElementById(
+    'callPerCallminuteEurope'
+  ).value
+  const internetPerGBEurope = document.getElementById(
+    'internetPerGBEurope'
+  ).value
+  const smsPerCountEurope = document.getElementById('smsPerCountEurope').value
+  const freeGBInternetCH = document.getElementById('freeGBInternetCH').value
+  const freeCallminutesCH = document.getElementById('freeCallminutesCH').value
+  const freeSMSCH = document.getElementById('freeSMSCH').value
+  const freeGBInternetEurope = document.getElementById(
+    'freeGBInternetEurope'
+  ).value
+  const freeCallminutesEurope = document.getElementById(
+    'freeCallminutesEurope'
+  ).value
+  const freeSMSEurope = document.getElementById('freeSMSEurope').value
+  const activationFee = document.getElementById('activationFee').value
+
+  const offer = {
+    offerId: offerId,
+    name: name,
+    provider: provider,
+    offerUrl: offerUrl,
+    basePrice: basePrice,
+    worldOffer: worldOffer,
+    cost: {
+      callPerCallminuteCH: callPerCallminuteCH,
+      internetPerGBCH: internetPerGBCH,
+      smsPerCountCH: smsPerCountCH,
+      callPerCallminuteEurope: callPerCallminuteEurope,
+      internetPerGBEurope: internetPerGBEurope,
+      smsPerCountEurope: smsPerCountEurope,
+    },
+    deductions: {
+      freeGBInternetCH: freeGBInternetCH,
+      freeCallminutesCH: freeCallminutesCH,
+      freeSMSCH: freeSMSCH,
+      freeGBInternetEurope: freeGBInternetEurope,
+      freeCallminutesEurope: freeCallminutesEurope,
+      freeSMSEurope: freeSMSEurope,
+    },
+    activationFee: activationFee,
+  }
+  const saveOfferURL = `https://handy-tarifvergleich-server.azurewebsites.net/offers/update?offerId=${offerId}`
+  try {
+    const response = await fetch(`${saveOfferURL}`, {
+      method: 'PUT',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+      },
+      body: JSON.stringify(offer),
+    })
+    if (!response.ok) {
+      throw new Error('Could not save offer (HTTP error)')
+    }
+    const data = await response.text()
+    if (response.status === 200) {
+      main.innerHTML = ''
+      window.location.hash = '#offer'
+    } else {
+      console.log('Save offer error:', data)
+    }
+  } catch (err) {
+    console.error(err)
+  }
 }
